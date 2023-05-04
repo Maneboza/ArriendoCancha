f9fb0ef18de078b9a3f978d1eec1b0ac84745ea7
Italo Contreras <icontrerasp@uni.pe>
2023-05-04 17:37:56 -0500

    add login form

--- backend/controllers/usuarios.controller.js
+++ backend/controllers/usuarios.controller.js
@@ -8,6 +8,7 @@ module.exports.index = (request, response) => {
 }
 
 module.exports.crearUsuario = async (request, response) => {
+    console.log("request",request)
     var requestData = request.body;
     try {
         var newUser = await UsuariosModel.create(requestData);    
@@ -26,8 +27,12 @@ module.exports.encontrarUsuarioId = async (request, response) => {
 }
 
 module.exports.loginUsuario = async (request, response) => {
+    
     var requestBody = request.body;
-    var usuarioEncontrado = await UsuariosModel.findOne({ email: "boza.marion@gmail.com", password: "12345678" });
+    console.log("requestBody",requestBody)
+    // var usuarioEncontrado = await UsuariosModel.findOne({ email: "boza.marion@gmail.com", password: "12345678" });
+    var usuarioEncontrado = await UsuariosModel.findOne(requestBody);
+    console.log("usuarioEncontrado",usuarioEncontrado)
     console.log(usuarioEncontrado);
     response.json({
         "status": "ok"
--- frontend/src/views/Login/Login.jsx
+++ frontend/src/views/Login/Login.jsx
@@ -3,27 +3,64 @@
 // import axios from 'axios';
 import "./Login.css";
 
+import { useNavigate } from "react-router-dom";
+import axios from 'axios'
+import { useState } from "react";
+
 import bgImage from "../../imagenes/fondos/imgFondo9.png";
 
 const Login = () => {
     // const [listaEventos, setListaEventos] = useState([{}]);
     // const navigate = useNavigate();
 
-    // useEffect(() => {
-    //     console.log("Inicio el componente");
-    //     axios.get("http://localhost:8000/eventos/listar").then(result => {
-    //         console.log(result.data);
-    //         setListaEventos(result.data);
-    //     });
-    // }, []);
+    const navigate = useNavigate() 
 
-    // const irAlCrear = () => {
-    //     navigate("/asistir/");
-    // }
+    const [email, setEmail] = useState('')
+    const [clave, setClave] = useState('')
+
+    async function login (ev) {
+        ev.preventDefault()
+        try {
+          console.log("login jsx")
+          //   const resp = await axios.post(window.$api + '/auth/login', {
+          //const resp = await axios.post('http://localhost:8000/api/auth/login', {  
+          const resp = await axios.post(window.$api + 'usuarios/login', {    
+            email, clave
+          })
+          // si el login fué exitoso, guardamos la credencial en localStorage
+          //   localStorage.setItem('token', resp.data.token)
+          // y redirigimos a la pantalla principal
+          navigate('/profesor/listar')
+        }
+        catch(err){
+            console.log("catch")
+          alert(err.response.data.error)
+          return
+        }
+    
+        
+    }
 
     return (
         <div >
             <img className="bg-img" src={bgImage} alt="Bg" />
+
+            <form className='login-input' onSubmit={login}>
+
+                <div className="form-group">
+
+                    <input required type="email" placeholder="EMAIL" className="form-control" id="email" onChange={ev => setEmail(ev.target.value)}/>
+                </div>
+
+                <div className="form-group">
+                    <input required type="password" placeholder="CLAVE" className="form-control" id="password" onChange={ev => setClave(ev.target.value)}/>
+                </div>  
+
+                <div className='login-button-out'>
+                    <button type="submit" className='login-button'>Ingresar</button>
+                </div>
+            </form>            
+
         </div>
     );
 }
